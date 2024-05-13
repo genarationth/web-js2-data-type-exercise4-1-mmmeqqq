9e161795b883748507a11532b6ce904e2efab298
mmmeqqq <bew.bussayakorn@gmail.com>
2024-05-13 14:33:11 +0700

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+let products = {
+    product_id : 'n1',
+     product_name : 'Aro',
+     quantity_in_stock : 6 ,
+     unit_price : 16 ,
+     product_description : {
+        product_brand: 'Aro',
+        size_or_quantity: '1.5L',
+        taste_or_scent:'null',
+        colour: 'clear',
+        additional_description: 'mineral from hill'
+    }
+};
+
+console.log(products.unit_price);
